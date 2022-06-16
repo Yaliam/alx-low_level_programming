4137e8eb903d609beb169f0714bc0e61f3a073e9
Yaliam <yahyaman87@gmail.com>
2022-06-16 08:24:36 -0700

    I fix

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strcat -> this is a function strcat
+*@dest: first param
+*src: second param
+*Return: a string
+*/
+char *_strcat(char *dest, char *src)
+{
+  strcat(dest, src)
+dest[len] = '\0';
+return (dest);
+}
