b8b7e4e74891a1d6c116bee69ddad997d7fea67d
Yaliam <yahyaman87@gmail.com>
2022-06-16 09:01:49 -0700

    i fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,19 +3,13 @@
 /**
 *_strcat -> this is a function strcat
 *@dest: first param
-*src: second param
+*@src: second param
 *Return: a string
 */
 char *_strcat(char *dest, char *src)
+
 {
-int len = 0, i;
-while (dest[len])
-len++;
-for (i = 0; src[i] != 0; i++)
-{
-dest[len] = src[i];
-len += 1;
-}
-dest[len] = '\0';
+
+strcat(dest, src);
 return (dest);
 }
